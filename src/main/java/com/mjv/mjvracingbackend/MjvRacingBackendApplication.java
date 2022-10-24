2ee101991515d7b5b2eccb6e86627a611ff8c22a
Julius Cavalcante <julius.c@outlook.com.br>
2022-10-24 13:43:17 -0300

    project created

---
+++ src/main/java/com/mjv/mjvracingbackend/MjvRacingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mjv.mjvracingbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MjvRacingBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MjvRacingBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mjv/mjvracingbackend/MjvRacingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mjv.mjvracingbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MjvRacingBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
