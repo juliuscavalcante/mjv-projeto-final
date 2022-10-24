b08e385fc898a1b1a2a49fbbf72412c991b8b5de
Julius Cavalcante <julius.c@outlook.com.br>
2022-10-24 13:53:44 -0300

    feat: enumerating profile, priority and status types

---
+++ src/main/java/com/mjv/mjvracingbackend/domain/enums/Priority.java
@@ -0,0 +1,39 @@
+package com.mjv.mjvracingbackend.domain.enums;
+
+public enum Priority {
+
+    LOW(0, "LOW_PRIORITY"),
+    MEDIUM(1, "MEDIUM_PRIORITY"),
+    HIGH(2, "HIGH_PRIORITY"),;
+
+    private Integer code;
+    private String description;
+
+
+    Priority(Integer code, String description) {
+        this.code = code;
+        this.description = description;
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public static Priority toEnum(Integer code) {
+        if (code == null) {
+            return null;
+        }
+
+        for (Priority pr : Priority.values()) {
+            if (code.equals(pr.getCode())) {
+                return pr;
+            }
+        }
+        throw new IllegalArgumentException("Invalid priority");
+    }
+}
+
---
+++ src/main/java/com/mjv/mjvracingbackend/domain/enums/Status.java
@@ -0,0 +1,37 @@
+package com.mjv.mjvracingbackend.domain.enums;
+
+public enum Status {
+
+    OPEN(0, "OPEN"),
+    IN_PROGRESS(1, "IN PROGRESS"),
+    CLOSED(2, "CLOSED");
+
+    private Integer code;
+    private String description;
+
+    Status(Integer code, String description) {
+        this.code = code;
+        this.description = description;
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public static Status toEnum(Integer code) {
+        if (code == null) {
+            return null;
+        }
+
+        for (Status s : Status.values()) {
+            if (code.equals(s.getCode())) {
+                return s;
+            }
+        }
+        throw new IllegalArgumentException("Invalid status");
+    }
+}
