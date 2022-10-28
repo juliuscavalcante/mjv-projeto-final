6754bc24b4a591335163bdb26f1e368081463a3b
Julius Cavalcante <julius.c@outlook.com.br>
2022-10-27 23:26:38 -0300

    feat: creation of EngineerDTO and creation of its constructor

---
+++ src/main/java/com/mjv/mjvracingbackend/domain/dto/EngineerDTO.java
@@ -0,0 +1,109 @@
+package com.mjv.mjvracingbackend.domain.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.mjv.mjvracingbackend.domain.entities.Engineer;
+import com.mjv.mjvracingbackend.domain.enums.Profile;
+
+import javax.validation.constraints.NotNull;
+import java.io.Serial;
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class EngineerDTO implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 4574898849434189782L;
+
+    protected Long id;
+
+    @NotNull(message = "name is required")
+    protected String  name;
+
+    @NotNull(message = "CPF is required")
+    protected String cpf;
+
+    @NotNull(message = "email is required")
+    protected String email;
+
+    @NotNull(message = "password is required")
+    protected String password;
+
+    protected Set<Integer> profiles = new HashSet<>();
+
+    @JsonFormat(pattern = "dd/MM/yyyy")
+    protected LocalDate birthDate;
+
+    public EngineerDTO(Engineer engineer) {
+        this.id = engineer.getId();
+        this.name = engineer.getName();
+        this.cpf = engineer.getCpf();
+        this.email = engineer.getEmail();
+        this.password = engineer.getPassword();
+        this.profiles = engineer.getProfiles().stream().map(Profile::getCode).collect(Collectors.toSet());
+        this.birthDate = engineer.getBirthDate();
+    }
+
+    public EngineerDTO() {
+        super();
+        addProfile(Profile.USER);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Set<Profile> getProfiles() {
+        return profiles.stream().map(Profile::toEnum).collect(Collectors.toSet());
+    }
+
+    public void addProfile(Profile profiles) {
+        this.profiles.add(profiles.getCode());
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+}
--- src/main/java/com/mjv/mjvracingbackend/domain/entities/Engineer.java
+++ src/main/java/com/mjv/mjvracingbackend/domain/entities/Engineer.java
@@ -1,12 +1,16 @@
 package com.mjv.mjvracingbackend.domain.entities;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.mjv.mjvracingbackend.domain.dto.EngineerDTO;
 import com.mjv.mjvracingbackend.domain.enums.Profile;
-import javax.persistence.*;
+
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
 import java.io.Serial;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Entity
 public class Engineer extends Person {
@@ -26,6 +30,16 @@ public class Engineer extends Person {
 
     public Engineer(Long id, String name, String cpf, String email, String password, LocalDate birthDate) {
         super(id, name, cpf, email, password, birthDate);
+    }
+
+    public Engineer(EngineerDTO engineerDTO) {
+        this.id = engineerDTO.getId();
+        this.name = engineerDTO.getName();
+        this.cpf = engineerDTO.getCpf();
+        this.email = engineerDTO.getEmail();
+        this.password = engineerDTO.getPassword();
+        this.profiles = engineerDTO.getProfiles().stream().map(Profile::getCode).collect(Collectors.toSet());
+        this.birthDate = engineerDTO.getBirthDate();
     }
 
     public List<Request> getRequests() {
