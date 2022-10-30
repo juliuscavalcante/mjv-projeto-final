79adf1142cd52c2255a487e303a6376526cdc874
Julius Cavalcante <julius.c@outlook.com.br>
2022-10-30 15:35:39 -0300

    fix: bug fix

--- src/main/java/com/mjv/mjvracingbackend/repository/PersonRepository.java
+++ src/main/java/com/mjv/mjvracingbackend/repository/PersonRepository.java
@@ -2,9 +2,11 @@ package com.mjv.mjvracingbackend.repository;
 
 import com.mjv.mjvracingbackend.model.entities.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface PersonRepository extends JpaRepository<Person, Long> {
 
     Optional<Person> findByCpf(String cpf);
