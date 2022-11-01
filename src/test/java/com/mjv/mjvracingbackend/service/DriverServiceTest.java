ab3c3caa8f5728e41c485fdc53235de9819c5719
Julius Cavalcante <julius.c@outlook.com.br>
2022-10-31 23:35:17 -0300

    test: creation of tests of delete, object not found and when already registered

--- src/test/java/com/mjv/mjvracingbackend/service/DriverServiceTest.java
+++ src/test/java/com/mjv/mjvracingbackend/service/DriverServiceTest.java
@@ -3,12 +3,15 @@ package com.mjv.mjvracingbackend.service;
 import com.mjv.mjvracingbackend.model.dto.DriverDTO;
 import com.mjv.mjvracingbackend.model.entities.Driver;
 import com.mjv.mjvracingbackend.repository.DriverRepository;
+import com.mjv.mjvracingbackend.service.exception.DataIntegrityViolationException;
+import com.mjv.mjvracingbackend.service.exception.ObjectNotFoundException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
+
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
@@ -17,7 +20,7 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @SpringBootTest
 class DriverServiceTest {
@@ -28,6 +31,10 @@ class DriverServiceTest {
     private static final String EMAIL = "hamilton@email.com";
     private static final String PASSWORD = "123";
     private static final LocalDate BIRTH_DATE = LocalDate.of(1996, Month.JANUARY,5);
+
+    private static final String OBJECT_NOT_FOUND = "driver id not found";
+    private static final String EMAIL_ALREADY_REGISTERED = "This CPF is already registered in our system";
+    private static final String CPF_ALREADY_REGISTERED = "This email is already registered in our system";
 
     @InjectMocks
     private DriverService driverService;
@@ -81,15 +88,82 @@ class DriverServiceTest {
     }
 
     @Test
-    void create() {
+    void whenFindByIdDriverThenReturnAnObjectNotFoundException() {
+        when(driverRepository.findById(anyLong())).thenThrow(new ObjectNotFoundException(OBJECT_NOT_FOUND));
+
+        try{
+            driverService.findById(ID);
+        } catch (Exception ex) {
+            assertEquals(ObjectNotFoundException.class, ex.getClass());
+            assertEquals(OBJECT_NOT_FOUND, ex.getMessage());
+        }
     }
 
     @Test
-    void update() {
+    void whenCreateThenReturnSuccess() {
+        when(driverRepository.save(any())).thenReturn(driver);
+
+        Driver response = driverService.create(driverDTO);
+
+        assertNotNull(response);
+        assertEquals(Driver.class, response.getClass());
+        assertEquals(ID, response.getId());
+        assertEquals(NAME, response.getName());
+        assertEquals(CPF, response.getCpf());
+        assertEquals(EMAIL, response.getEmail());
+        assertEquals(PASSWORD, response.getPassword());
+        assertEquals(BIRTH_DATE, response.getBirthDate());
     }
 
     @Test
-    void delete() {
+    void whenCreateThenReturnAnDataIntegrityViolationExceptionBecauseCPFAlreadyRegistered() {
+        when(driverRepository.findByCpf(anyString())).thenReturn(driverOptional);
+
+        try {
+            driverOptional.get().setId(1L);
+            driverService.create(driverDTO);
+        } catch (Exception ex) {
+            assertEquals(DataIntegrityViolationException.class, ex.getClass());
+            assertEquals(CPF_ALREADY_REGISTERED , ex.getMessage());
+        }
+    }
+
+    @Test
+    void whenCreateThenReturnAnDataIntegrityViolationExceptionBecauseEmailAlreadyRegistered() {
+        when(driverRepository.findByEmail(anyString())).thenReturn(driverOptional);
+
+        try {
+            driverOptional.get().setId(1L);
+            driverService.create(driverDTO);
+        } catch (Exception ex) {
+            assertEquals(DataIntegrityViolationException.class, ex.getClass());
+            assertEquals(EMAIL_ALREADY_REGISTERED , ex.getMessage());
+        }
+    }
+
+    /*@Test
+    void whenUpdateThenReturnSuccess() {
+        when(driverRepository.save(any())).thenReturn(driver);
+        when(driverRepository.findById(anyLong())).thenReturn(driverOptional);
+
+        Driver response = driverService.update(ID, driverDTO);
+
+        assertNotNull(response);
+        assertEquals(Driver.class, response.getClass());
+        assertEquals(ID, response.getId());
+        assertEquals(NAME, response.getName());
+        assertEquals(CPF, response.getCpf());
+        assertEquals(EMAIL, response.getEmail());
+        assertEquals(PASSWORD, response.getPassword());
+        assertEquals(BIRTH_DATE, response.getBirthDate());
+    }*/
+
+    @Test
+    void deleteWithSuccess() {
+        when(driverRepository.findById(anyLong())).thenReturn(driverOptional);
+        doNothing().when(driverRepository).deleteById(anyLong());
+        driverService.delete(ID);
+        verify(driverRepository, times(1)).deleteById(anyLong());
     }
 
 
